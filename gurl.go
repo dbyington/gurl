bcd8504232d055ecb88a7fc20e437ba6a32211ca
Don Byington <don@dbyington.com>
2018-03-31 17:59:06 -0600

    Validate host, exit where needed

--- gurl.go
+++ gurl.go
@@ -4,9 +4,35 @@ import (
 	"fmt"
 	"github.com/jessevdk/go-flags"
 	"io/ioutil"
+	"log"
+	"net"
 	"net/http"
 	"net/url"
+	"os"
 )
+
+var mylog = log.New(os.Stderr, "app: ", log.LstdFlags|log.Lshortfile)
+
+func ValidateURL(u *url.URL) (*url.URL, error) {
+
+	if u.Scheme == "" {
+		u.Scheme = "http"
+		u, _ = u.Parse(u.String())
+	}
+
+	if u.Host == "" {
+		mylog.Printf("URL must include a valid host: %s\n", u.Host)
+		os.Exit(1)
+
+	} else {
+		addr, err := net.LookupHost(u.Host)
+		if err != nil {
+			mylog.Println(u.Host, addr)
+			return &url.URL{}, fmt.Errorf("Failed: %s", err.Error())
+		}
+	}
+	return u, nil
+}
 
 type CommandFlags struct {
 	User    string   `short:"u" long:"user" description:"HTTP Basic Auth username"`
@@ -19,18 +45,18 @@ type CommandFlags struct {
 	} `positional-args:"yes" required:"yes"`
 }
 
-type Usage interface {
-	Usage() string
-}
-
-func (c CommandFlags) Usage() string {
-	return "usage"
-}
 func main() {
 	var commandFlags CommandFlags
 	flagsParser := flags.NewParser(&commandFlags, flags.Default)
 	flagsParser.Usage = "options"
-	flagsParser.Parse()
+	if _, err := flagsParser.Parse(); err != nil {
+		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
+			os.Exit(0)
+		} else {
+			flagsParser.WriteHelp(os.Stdout)
+			os.Exit(1)
+		}
+	}
 
 	if len(commandFlags.Args.Rest) > 0 {
 		fmt.Println("Extra args:")
@@ -42,17 +68,25 @@ func main() {
 	url, err := url.Parse(commandFlags.Args.URL)
 	if err != nil {
 		fmt.Errorf("Bad URL provided; %s: %s\n", commandFlags.Args.URL, err.Error())
+		os.Exit(1)
 	}
 
+	url, err = ValidateURL(url)
+	if err != nil {
+		mylog.Printf("URL failed validation: %s", err.Error())
+		os.Exit(1)
+	}
 	response, err := http.Get(url.String())
 	if err != nil {
 		fmt.Errorf("Failed to GET; %s: %s\n", commandFlags.Args.URL, err.Error())
+		os.Exit(1)
 	}
 
 	body, err := ioutil.ReadAll(response.Body)
 	response.Body.Close()
 	if err != nil {
 		fmt.Errorf("Failed to read response body: %s\n", err.Error())
+		os.Exit(1)
 	}
 
 	fmt.Println(string(body))
